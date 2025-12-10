35a3c59ea80f0b64457423a6c4434a31b24c4b8d
LouLFBV <lou.lefebvre@ynov.com>
2025-12-10 09:12:59 +0100

    commit

--- Assets/Scripts/AttackBehaviour.cs
+++ Assets/Scripts/AttackBehaviour.cs
@@ -3,34 +3,31 @@ using UnityEngine;
 public class AttackBehaviour : MonoBehaviour
 {
     [SerializeField] private Camera playerCamera;
-
-    public bool canShoot = false;
-    public bool chargeBow;
     public Weapon weaponUsed;
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (weaponUsed != null && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            ShootArrow();
+            Shoot();
         }
     }
 
-    public void ShootArrow()
+    public void Shoot()
     {
         Debug.Log("Tire");
 
         AlignArrowSpawnToCamera();
-        Debug.DrawRay(weaponUsed.shootPoint.position, weaponUsed.shootPoint.forward * weaponUsed.range, Color.red);
-        if (Physics.Raycast(weaponUsed.shootPoint.position, weaponUsed.shootPoint.forward, out RaycastHit hit, weaponUsed.range))
+        Debug.DrawRay(weaponUsed.shootPoint.position, weaponUsed.shootPoint.forward * weaponUsed.weaponData.range, Color.red);
+        if (Physics.Raycast(weaponUsed.shootPoint.position, weaponUsed.shootPoint.forward, out RaycastHit hit, weaponUsed.weaponData.range))
         {
             if (hit.collider.CompareTag("Player"))
             {
                 Debug.Log("Hit " + hit.collider.name);
                 if (hit.transform.TryGetComponent<PlayerStats>(out var enemy))
                 {
-                    enemy.TakeDamage(weaponUsed.damage);
+                    enemy.TakeDamage(weaponUsed.weaponData.damage);
                 }
             }
             else if (hit.collider.CompareTag("Descrutable"))
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,7 +2,23 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public WeaponData weaponData;
     public Transform shootPoint;
-    public int damage = 10;
-    public int range = 100;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Interact.instance.canInteract = true;
+            Interact.instance.currentWeapon = this;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Interact.instance.canInteract = false;
+            Interact.instance.currentWeapon = null;
+        }
+    }
 }
