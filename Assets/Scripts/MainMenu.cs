d6a9441e2222f50a00726d8a36ab0e5bcf20508c
LouLFBV <lou.lefebvre@ynov.com>
2026-05-20 14:04:12 +0200

    push

--- Assets/Scripts/MainMenu.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class MainMenu : MonoBehaviour
-{
-    public void ButtonLoadScene(string sceneName)
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
-    }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-Convention de nommage utilisée : camelCase pour les champs/variables, PascalCase pour les Class/méthodes
